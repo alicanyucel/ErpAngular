cfe3f00070179cc06bdc0257f837017130b96a44
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-06-14 14:16:42 +0300

    ADMİN LTE EKLENDİ

--- src/app/components/blank/blank.component.ts
+++ src/app/components/blank/blank.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blank',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.css'
 })
 export class BlankComponent {
-
+  @Input() pageName: string = "";
+  @Input() routes: string[] = [];
 }
--- src/app/components/layouts/layouts.component.ts
+++ src/app/components/layouts/layouts.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layouts',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layouts.component.html',
   styleUrl: './layouts.component.css'
 })
